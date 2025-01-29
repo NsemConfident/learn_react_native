ff6b21921bef7c513875c04a5853799dc3fa797e
unknown <njocknsem@gmail.com>
2025-01-29 18:44:19 +0100

    activity indicator and aler

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,9 @@ import {
   Button,
   Pressable,
   Modal,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
 import { useState } from "react";
 import React from "react";
@@ -18,20 +21,16 @@ const index = () => {
   return (
     <View style={{ flex: 1, backgroundColor: "#004e4e", padding: 60 }}>
       {/* ================================TEXT AND IMAGE================================= */}
-
       {/* <View style={{width: 200, height: 300, backgroundColor: "#3e3e3e"}}></View>
       <Text style={{padding: 60}}>index <Text style={{color:"white", padding: 50}}>Nested text</Text></Text>
       <Image source={{uri: 'https://ca.slack-edge.com/T05R6LXN7J8-U06R7GRBJSD-0bb90b6f36d1-512'}} style={{width: 200, height: 200}}/> */}
-
       {/* =====================================IMAGE BACKGROUND======================================= */}
       {/* adding image background */}
       {/* <ImageBackground source={logoImage} style={{flex: 1}}>
         <Text style={{padding: }}>this is the home page</Text>
       </ImageBackground> */}
-
       {/* ==================================SCROLL VIEW=================================== */}
       {/* addgin scrollview */}
-
       {/* <ScrollView>
         <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut excepturi
@@ -105,10 +104,8 @@ const index = () => {
           ut iure id aut at?
         </Text>
       </ScrollView> */}
-
       {/* ===========================================BUTTON================================================== */}
       {/* Button componenet: note that it is a self closing tag */}
-
       {/* <Button
         title="press"
         onPress={() => {
@@ -117,7 +114,6 @@ const index = () => {
         color="midnightblue"
         disabled
       /> */}
-
       {/* ========================================PRESSABLE===================================================== */}
       {/* using pressable  */}
       {/* <Pressable
@@ -127,9 +123,7 @@ const index = () => {
       >
         <Image source={logoImage} style={{ width: 100, height: 100 }} />
       </Pressable> */}
-
       {/* ========================================MODAL===================================================== */}
-
       {/* using modal component  */}
       {/* <Button
         title="press"
@@ -160,14 +154,67 @@ const index = () => {
           />
         </View>
       </Modal> */}
-
       {/* ====================================STATUS BAR======================================= */}
       {/* 
       StatusBar allows you to control your application status bar,
        which is the area located at the top of the screen.
 
-      it displays various information such as the current time, wifi and network information battery level and status icon
+      it displays various information such as the current time, wifi and network information battery level and other status icon
       */}
+      {/* <StatusBar
+        backgroundColor="lightgreen"
+        barStyle="default" //to change the text of the status bar. defatult corresponds black text for ios and white for android
+        //barStyle could take value: dark-content(black text on both), light-content(white text on both)
+        // hidden //hide status bar. the recommended approach is to use a state for animating
+      /> */}
+
+      {/* ===========================================ACTIVE INDICATOR======================================= */}
+
+      {/* -it displays a circular loading indicator
+      -used to inform user about the status of ongoin processes loading an app, submittin a form, saving updates */}
+
+      {/* <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator
+        size="large"
+        color="midnightblue"
+        animating={false}
+      />{" "} */}
+
+      {/* the recommended approach is to use a state for animating */}
+
+      {/* ===========================================ALERT======================================= */}
+
+      {/* -it serves more like an api than a typical ui component 
+      -rather than redering it as part of jsx, you invoke methods that generate ui elements
+      -you can launch an alert dialog with a spercified title and message
+      -additionally, you have the option to include a list of buttons */}
+
+      {/* <Button title="Alert" onPress={() => Alert.alert("Invalid data!")} />
+      <Button
+        title="Alert"
+        onPress={() => Alert.alert("Invalid data!", "Your data is invalid")}
+      />
+      <Button
+        title="Alert"
+        onPress={() =>
+          Alert.alert("Invalid data!", "Your data is invalid", [
+            {
+              text: "cancel",
+              onPress: () => {
+                console.log("cancel pressed");
+              },
+            },
+            {
+              text: "Ok",
+              onPress: () => {
+                console.log("Ok pressed");
+              },
+            },
+          ])
+        }
+      /> */}
     </View>
   );
 };
